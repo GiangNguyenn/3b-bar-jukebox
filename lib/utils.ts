d1dfd4d162b0332cd46e71af73bc2e0039c96886
Jeremy Mullaly <manack@gmail.com>
2025-03-21 08:49:44 +0700

    Remove items from the queue that have already been played

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { TrackItem } from "@/shared/types"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const filterUpcomingTracks = (
+  playlistTracks: TrackItem[],
+  currentTrackId: string | null
+): TrackItem[] => {
+  if (!currentTrackId) return playlistTracks; // If no track is playing, return full list
+
+  const index = playlistTracks.findIndex(track => track.track.id === currentTrackId);
+  
+  if (index === -1) return playlistTracks; // If current track isn't found, return full list
+
+  return playlistTracks.slice(index + 1); // Return only upcoming songs
+};
