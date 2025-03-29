1098d396f388f5c64439c41648eba6c850900d47
Jeremy Mullaly <manack@gmail.com>
2025-03-29 14:46:26 +0700

    Thinking that not everything in the world needs to be a hook.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,8 @@ import { Playlist } from "@/components/Playlist/Playlist";
 import Loading from "./loading";
 import SearchInput from "@/components/SearchInput";
 import { useDebounce } from "use-debounce";
-import { useUnfollowOldPlaylists } from "@/hooks/useUnfollowOldPlaylists";
+import { cleanupOldPlaylists } from "@/services/playlist";
+import { useMyPlaylists } from "@/hooks/useMyPlaylists";
 
 export default function Home() {
   const { createPlaylist, todayPlaylistId } = useCreateNewDailyPlaylist();
@@ -18,9 +19,14 @@ export default function Home() {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState<TrackDetails[]>([]);
   const { searchTracks } = useSearchTracks();
+  const { data: playlists } = useMyPlaylists();
   
-  // Handle unfollowing of old playlists
-  useUnfollowOldPlaylists();
+  // Handle cleanup of old playlists
+  useEffect(() => {
+    if (playlists?.items) {
+      cleanupOldPlaylists(playlists.items).catch(console.error);
+    }
+  }, [playlists?.items]);
 
   useEffect(() => {
     (async () => {
