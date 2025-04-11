e7fd3a7ad694bec1d2b3f3ba05ad2327924c1b8a
Jeremy Mullaly <manack@gmail.com>
2025-04-11 19:28:04 +0700

    feat: add debounced playlist state refresh in useSpotifyPlayerState

--- lib/utils.ts
+++ lib/utils.ts
@@ -29,3 +29,16 @@ export const filterUpcomingTracks = (
 
   return upcomingTracks
 }
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null = null
+  return (...args: Parameters<T>) => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => func(...args), wait)
+  }
+}
