0b151373d3413a6f36d13936854c79e6be268c38
Jeremy Mullaly <manack@gmail.com>
2025-04-11 17:38:11 +0700

    Fixing Linter errors

---
+++ types/spotify.d.ts
@@ -0,0 +1,24 @@
+declare global {
+  interface Window {
+    Spotify: {
+      Player: new (options: SpotifyPlayerOptions) => SpotifyPlayerInstance
+    }
+    spotifyPlayerInstance: SpotifyPlayerInstance | null
+    refreshSpotifyPlayer: () => Promise<void>
+  }
+}
+
+export interface SpotifyPlayerOptions {
+  name: string
+  getOAuthToken: (callback: (token: string) => void) => void
+  volume: number
+}
+
+export interface SpotifyPlayerInstance {
+  connect(): Promise<boolean>
+  disconnect(): Promise<void>
+  addListener(eventName: string, callback: (event: any) => void): void
+  removeListener(eventName: string, callback: (event: any) => void): void
+}
+
+export {} 
