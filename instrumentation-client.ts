206835335f9e8bf8430ba75c024b18cf1fef6b37
Jeremy Mullaly <manack@gmail.com>
2025-06-01 13:57:23 +1000

    Prettier

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,15 +2,13 @@
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://dcd38d2e63ff7bbc09f9b88743d46b0b@o4509417600450560.ingest.de.sentry.io/4509417600843856",
+  dsn: 'https://dcd38d2e63ff7bbc09f9b88743d46b0b@o4509417600450560.ingest.de.sentry.io/4509417600843856',
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -24,7 +22,7 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+  debug: false
+})
 
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
